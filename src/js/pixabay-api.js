e872bf8e4db5e75a7a2971b057283daff2d6ed42
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-05-13 23:49:29 +0300

    реализовал get запрос

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default function fetchData(query) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: '50268892-929648ae4af930c873d247de9',
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => {
+      return response.data;
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import fetchData from './js/pixabay-api';
-import moduleName from './js/render-functions';
+// import moduleName from './js/render-functions';
+
+// const formEl = document.querySelector('.form');
+
+// formEl.addEventListener;
+
+fetchData()
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
