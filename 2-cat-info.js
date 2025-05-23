b455c4b6e5b87ca9a2e41b6351afb225bbec2e12
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-05-23 20:15:30 +0000

    Deploying to gh-pages from @ DmitryKishkanev/goit-js-hw-11@aaf918ed9efcb5b11ae278ec7ef242d7ae20be67 🚀

--- 2-cat-info.js
+++ 2-cat-info.js
@@ -1,4 +1,11 @@
-import"./assets/styles-DXaELHI0.js";import{a as c}from"./assets/vendor-DAobNtaX.js";const a="live_ gCrMzxVedQewgspyzOBQIG2uzIc7uX ZZLlOa2KWOyd9IsmmF2onJcOmaXJlO sQfP";c.defaults.headers.common["x-api-key"]=a;function r(){return c.get("https://api.thecatapi.com/v1/breeds").then(e=>e.data)}function i(e){return c.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${e}`).then(t=>t)}const o=document.querySelector(".breed-select"),s=document.querySelector(".cat-info");o.addEventListener("change",l);r().then(e=>{d(e)}).catch(e=>{console.log(e)});function d(e){e.forEach(t=>{const n=document.createElement("option");n.value=t.id,n.textContent=t.name,o.appendChild(n)})}function l(){const e=o.value;i(e).then(t=>{console.log(t),u(t.data)})}function u(e){const t=`
-  <img src="${e.url}" alt="" class="cat-img" />
-`;s.insertAdjacentHTML("beforeend",t)}
+import"./assets/styles-aT9tsAiQ.js";import{a as c,i as u}from"./assets/vendor-Do1ajUWv.js";const f="live_ gCrMzxVedQewgspyzOBQIG2uzIc7uX ZZLlOa2KWOyd9IsmmF2onJcOmaXJlO sQfP";c.defaults.headers.common["x-api-key"]=f;function g(){return c.get("https://api.thecatapi.com/v1/breeds").then(e=>e.data)}function v(e){return c.get(`https://api.thecatapi.com/v1/breeds/${e}`).then(t=>t.data)}function L(e){return c.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${e}`).then(t=>t)}const d=document.querySelector(".loader");function y(e,t){t.classList.remove("is-hidden"),e.forEach(a=>{const s=document.createElement("option");s.value=a.id,s.textContent=a.name,t.appendChild(s)})}function C(e,t,a,s){const p=e.data[0];v(t.value).then(n=>{if(!n){a.innerHTML="<p>Информация о породе недоступна.</p>";return}const m=`
+<div class="cat-card">
+  <img src="${p.url}" alt="${n.name}" class="cat-img" />
+  <div class="cat-description">
+    <h2>${n.name}</h2>
+    <p><strong>Temperament:</strong> ${n.temperament}</p>
+    <p><strong>Description:</strong> ${n.description}</p>
+  </div>
+</div>
+    `;a.classList.remove("is-hidden"),a.innerHTML=m}).catch(n=>{s(n)})}function l(){d.classList.add("active")}function h(){d.classList.remove("active")}const o=document.querySelector(".breed-select"),r=document.querySelector(".cat-info");o.addEventListener("change",O);o.classList.add("is-hidden");l();g().then(e=>{y(e,o)}).catch(e=>{i(),o.classList.add("is-hidden")}).finally(()=>{h()});function O(){r.classList.add("is-hidden"),l();const e=o.value;L(e).then(t=>{C(t,o,r,i)}).catch(t=>{i()}).finally(()=>{h()})}function i(){return u.show({message:"Oops! Something went wrong! Try reloading the page!",messageColor:"white",backgroundColor:"red",icon:"	fa fa-ban",iconColor:"#8b0000",position:"topRight"})}
 //# sourceMappingURL=2-cat-info.js.map
