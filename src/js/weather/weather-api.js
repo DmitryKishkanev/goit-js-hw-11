5a197081e72e4bcd8fc4376b5752c7479d98407d
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-06-02 22:00:26 +0300

    сделал запрос за данными

--- src/js/weather/weather-api.js
+++ src/js/weather/weather-api.js
@@ -1,1 +1,24 @@
 import axios from 'axios';
+
+// http://api.weatherapi.com/v1/forecast.json?key=b33a3ee3df124911b63162045252805&q=Paris&days=5
+
+export function fetchWeather(city, days) {
+  const BASE_URL = 'http://api.weatherapi.com/v1';
+  const API_KEY = 'b33a3ee3df124911b63162045252805';
+
+  return axios
+    .get(`${BASE_URL}/forecast.json`, {
+      params: {
+        key: API_KEY,
+        q: city,
+        days: days,
+      },
+    })
+    .then(response => {
+      return response;
+    });
+
+  //   fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`).then(
+  //     resp => console.log(resp)
+  //   );
+}
--- src/js/weather/weather-main.js
+++ src/js/weather/weather-main.js
@@ -1,1 +1,6 @@
+import { fetchWeather } from './weather-api';
 import '../../css/styles.css';
+
+fetchWeather('Paris', 5)
+  .then(data => console.log(data))
+  .catch(window.alert('statusText'));
