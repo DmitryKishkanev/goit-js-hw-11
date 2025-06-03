f0777fdf131495aef5e80e90b925e4eacb9d83f9
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-06-03 19:24:18 +0000

    Deploying to gh-pages from @ DmitryKishkanev/goit-js-hw-11@e53ae0efe66a47476a354f5d87bba19e9788c61e 🚀

--- 3-weather.js
+++ 3-weather.js
@@ -1,2 +1,7 @@
-import"./assets/styles-Bx2iRc3n.js";import{a as o}from"./assets/vendor-DAobNtaX.js";function r(t,e){return o.get("http://api.weatherapi.com/v1/forecast.json",{params:{key:"b33a3ee3df124911b63162045252805",q:t,days:e}}).then(a=>a)}r("Paris",5).then(t=>console.log(t)).catch(window.alert("statusText"));
+import"./assets/styles-Bx2iRc3n.js";function o(e,r){return fetch(`http://api.weatherapi.com/v1/forecast.json?key=b33a3ee3df124911b63162045252805&q=${e}&days=${r}`).then(n=>{if(!n.ok)throw new Error(n.statusText);return n.json()})}const a=document.querySelector(".js-search"),s=document.querySelector(".js-list");a.addEventListener("submit",i);function i(e){e.preventDefault();const{query:r,days:c}=e.currentTarget.elements;o(r.value,c.value).then(t=>s.innerHTML=u(t.forecast.forecastday)).catch(t=>console.log(t))}function u(e){return e.map(({date:r,day:{avgtemp_c:c,condition:{icon:t,text:n}}})=>`<li>
+        <img src="${t}" alt="${n}" />
+        <p>${n}</p>
+        <h2>${r}</h2>
+        <h3>${c}</h3>
+      </li>`).join("")}
 //# sourceMappingURL=3-weather.js.map
